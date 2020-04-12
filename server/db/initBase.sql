cda888746676e34ee1e698856a6cdb3c5f151a3a
miaoulelion <nicolas-berthier@gmx.fr>
2020-04-12 19:00:55 +0200

    ajout service & modif initbdd

--- server/db/initBase.sql
+++ server/db/initBase.sql
@@ -1,6 +1,4 @@
-
 DROP TABLE IF EXISTS USERS;
-
 CREATE TABLE USERS (
   ID SERIAL PRIMARY KEY,
   username VARCHAR(25),
@@ -16,10 +14,7 @@ VALUES ('toto','Dublanc','Bernard','bernard@wanadoo.fr','blabla');
 INSERT INTO USERS (username, firstname, lastname, email, encrypted_password)
 VALUES ('Gilbert','Dublanc','Bernard','bernard@wanadoo.fr','blabla');
 
-
-
-DROP TABLE IF EXISTS LISTE;
-
+DROP TABLE IF EXISTS LISTE CASCADE;
 CREATE TABLE LISTE(
     ID SERIAL PRIMARY KEY,
     username VARCHAR(25), -- Nom de l'utilisateur à qui appartient la liste de tâches
@@ -28,17 +23,13 @@ CREATE TABLE LISTE(
     --je mets le jour de création c'est gratuit
 );
 
-
-
-INSERT INTO LISTE (username, titre) VALUES ('toto', 'Plan pour battre GrosChibrax7');
+INSERT INTO LISTE (username, titre) VALUES ('toto', 'Plan pour vénérer GrosChibrax7');
 INSERT INTO LISTE (username, titre) VALUES ('Gilbert', 'Terminer le projet');
 
-
-DROP TABLE IF EXISTS TACHE;
-
+DROP TABLE IF EXISTS TACHE CASCADE;
 CREATE TABLE TACHE(
     ID SERIAL PRIMARY KEY,
-    idListe INTEGER REFERENCES LISTE(ID),
+    idListe INTEGER REFERENCES LISTE(ID) ON DELETE CASCADE,
     titre VARCHAR(60), 
     echeance DATE, -- Date d'échéance de la tâche
     note VARCHAR(300), -- Texte de description de la tâche
@@ -46,30 +37,19 @@ CREATE TABLE TACHE(
     created_at timestamp default current_timestamp
 );
 
+INSERT INTO TACHE (idListe, titre, echeance, note, fait) 
+VALUES (1, 'Battre PtitChibrax', '2020-05-18','Entrainer sa brute tous les jours', FALSE);
 
-INSERT INTO TACHE (idListe,titre, echeance, note,fait) 
-VALUES (1,'Entrainement brute','2020-04-20','Ouvrir firefox tous les jours',FALSE);
-/*
-
-DROP TABLE IF EXISTS SOUSTACHE;
-
-
+DROP TABLE IF EXISTS SOUSTACHE CASCADE;
 CREATE TABLE SOUSTACHE(
     ID SERIAL PRIMARY KEY,
-    idTache REFERENCES TACHE(ID),
+    idTache INTEGER REFERENCES TACHE(ID) ON DELETE CASCADE,
     titre VARCHAR(60),
-    note VARCHAR(300), --La sous-tâche a-t-elle des notes ?
     fait BOOLEAN, 
     created_at timestamp default current_timestamp
 );
 
-
-INSERT INTO SOUSTACHE (idTache, titre, note, fait) 
-VALUES (0, 'Rythme','Faire les trois matchs par jour', FALSE);
-INSERT INTO SOUSTACHE (idTache, titre, note, fait) 
-VALUES (0, 'Tactique','Choisir les adversaires les plus faibles', FALSE);
-
-
-*/
-
-
+INSERT INTO SOUSTACHE (idTache, titre, fait) 
+VALUES (1, 'Rythme', FALSE);
+INSERT INTO SOUSTACHE (idTache, titre, fait) 
+VALUES (1, 'Tactique', FALSE);
---
+++ server/services/liste.js
@@ -0,0 +1,25 @@
+const utils = require("../db/utils");
+
+
+module.exports = {
+    getAll,
+    getById,
+    save,
+    update,
+    deleteById
+  };
+
+
+  function getAll(sortBy, callback) {
+    const query = 
+    `SELECT  l.id, l.username, t.id, t.titre, t.echeance, t.note, t.fait, st.id, st.titre, st.fait
+    FROM liste l, tache t, soustache st
+    WHERE l.id=t.id AND t.id=st.id;`;
+    utils.executeQuery(query, [], (err, result) => {
+      if (err) {
+        callback(true, err);
+      } else {
+        callback(undefined, result.rows);
+      }
+    });
+  }
