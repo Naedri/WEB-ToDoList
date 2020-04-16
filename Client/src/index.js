d6330a2c977906c494761c1d1f0d5bbdeb541561
Naedri <adrien.jallais@protonmail.com>
2020-04-16 17:32:57 +0200

    css index

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+import "./css/index.css";
 
 ReactDOM.render(
     <BrowserRouter>
