991106d546ece1c9fe96bae4869ccbf814b217e3
miaoulelion <nicolas-berthier@gmx.fr>
2020-04-17 17:33:43 +0200

    ajout routes soustaches api

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -17,6 +17,7 @@ const router = express.Router();
 
 /* router for Lists and tasks *****************************/
 
+
 //Retourner toutes les listes
   router.get("/api/everything/", (req, res) => {
     ServiceListe.getAll((err, result)=>{
@@ -28,17 +29,53 @@ const router = express.Router();
       });
   });
 
-  //Retourne toutes les sous-tâches
-  router.get("/soustache", (req, res) => {
-    ServiceSousTache.getAll((err, result)=>{
-        if(err){
-            res.status(500).json({ message: err });
-        }else{
-            res.json(result);
-        }
-      });
+
+//Créer une nouvelle liste à partir d'un titre envoyé par le body
+//On renvoie en Json le l'id de la nouvelle liste
+router.post("/lists", (req, res) => {
+  ServiceListe.create(req.body.titre ,(err, result)=>{
+      if(err){
+          console.log(result);
+          res.status(500).json({ message: err });
+      }else{
+          console.log(result);
+          res.json(result);
+      }
+    });
+});
+
+
+
+  //retourne une liste en particulier (par ID)
+  router.get("/lists/:id([0-9]*)", (req, res) => {
+    ServiceListe.getById(req.params.id, (err, result) => {
+      if (err) {
+        res.status(500).send();
+        console.log("erreur 500");
+      }
+      else{
+        console.log(result);
+        res.json(result);
+      }
+    });
   });
 
+
+  //Supprime une liste et toutes ses tâches et sous-tâches
+  router.delete("/lists/:id([0-9]*)",(req, res) => {
+    ServiceListe.deleteById(req.params.id, (err, result) => {
+      if (err) {
+        res.status(500).send();
+        console.log("erreur 500");
+      }
+      else{
+        console.log("Liste supprimée : "+req.params.id);
+        res.json(req.params.id);
+      }
+    });
+  });
+
+//################################### TACHES
 
   // tâche ajouter supprimer modifier
 
@@ -90,47 +127,74 @@ const router = express.Router();
   });
 
 
-//retourne une liste en particulier (par ID)
-  router.get("/lists/:id([0-9]*)", (req, res) => {
-    ServiceListe.getById(req.params.id, (err, result) => {
-      if (err) {
-        res.status(500).send();
-        console.log("erreur 500");
-      }
-      else{
-        console.log(result);
-        res.json(result);
-      }
-    });
-  });
 
-//Supprime une liste et toutes ses tâches et sous-tâches
-  router.delete("/lists/:id([0-9]*)",(req, res) => {
-    ServiceListe.deleteById(req.params.id, (err, result) => {
-      if (err) {
-        res.status(500).send();
-        console.log("erreur 500");
-      }
-      else{
-        console.log("Liste supprimée : "+req.params.id);
-        res.json(req.params.id);
-      }
-    });
-  });
+  //################################### SOUS TACHES
+//sous tâche ajouter supprimer modifier
 
-//Créer une nouvelle liste à partir d'un titre envoyé par le body
-//On renvoie en Json le l'id de la nouvelle liste
-  router.post("/lists", (req, res) => {
-    ServiceListe.create(req.body.titre ,(err, result)=>{
+  //Retourne toutes les sous-tâches
+  router.get("/soustache", (req, res) => {
+    ServiceSousTache.getAll((err, result)=>{
         if(err){
-            console.log(result);
             res.status(500).json({ message: err });
         }else{
-            console.log(result);
             res.json(result);
         }
       });
   });
+
+
+router.post("/soustache", (req, res) => {
+  const infoCreaSouSTache={
+    ...req.body
+  }
+  console.log(infoCreaSouSTache);
+  ServiceTache.create(infoCreaSouSTache ,(err, result)=>{
+    if(err){
+        console.log(result);
+        res.status(500).json({ message: err });
+    }else{
+        res.json(result);
+    }
+  });
+});
+
+
+router.patch("/soustache/:id([0-9]*)", (req, res) => {
+  const infoSouSTache={
+    ...req.body,
+    idTache: req.params.id
+  }
+  ServiceTache.update(infoSouSTache,(err, result)=>{
+      if(err){
+          console.log(result);
+          res.status(500).json({ message: err });
+      }else{
+          console.log("MAJ de la soustache : "+req.params.id);
+          res.json(req.params.id);
+      }
+    });
+});
+
+router.delete("/soustache/:id([0-9]*)", (req, res) => {
+  ServiceTache.deleteById(req.params.id, (err, result) => {
+    if (err) {
+      res.status(500).send();
+      console.log("erreur 500");
+    }
+    else{
+      console.log("Liste supprimée : "+req.params.id);
+      res.json(req.params.id);
+    }
+  });
+});
+
+
+
+
+
+
+
+
 
 
 /* router for user *****************************/
